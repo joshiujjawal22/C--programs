d105a45a6b59e14c051f61d5418785e30144caf8
UJJAWAL JOSHI <joshiujjawal22@gmail.com>
2019-08-31 10:47:40 +0530

    Create Alice and bob's silly game.c
    Input:5
    Output:Alice
    Explanation:We are given n=5 , so set={1,2,3,4,5} Alice chooses the prime number =2 and deletes the numbers2 and 4  and from the set, which becomes set={1,3,5}. Now there are two primes left,3 and 5  and . Bob can remove either prime from the set, and then Alice can remove the remaining prime. Because Bob is left without a final move, Alice will always win. Thus, we print Alice on a new line.

---
+++ Alice and bob's silly game.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{ int n;
+scanf("%d",&n);
+   int i,j,c,k;
+    int a[n+1];
+    int main_count=0;
+    for(i=1;i<=n;i++)
+    {
+        a[i]=i;
+    }
+   for(i=1;i<=n;i++)
+   {    c=0;
+   if(a[i]>1){
+       for(j=1;j<=n;j++)
+       {
+           if(a[i]%j==0) c++;
+
+
+       }
+       if(c==2) {
+           main_count++;
+           for(k=1;k<=n;k++)
+           {
+               if(a[k]%a[i]==0) if(k!=i) a[k]=1;
+          
+           }
+           a[i]=1;
+           
+       }
+   }
+}
+if(main_count%2==0) printf("Bob");
+    else return printf("Alice");
+}
